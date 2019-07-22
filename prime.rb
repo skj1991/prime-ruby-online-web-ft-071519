81afbf7b97568470bed095fa9474f2f008975e12
Stacy Johns <stacykjohns@icloud.com>
2019-07-22 23:49:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(x)
+def prime?(n)
   count = 0
-
-  for each in (2..x)
-    x % each == 0
+  
+  for each in (2..n)
+    n % each == 0 ? count += 1 : nil
   end
-
-  return count == 1 
+  
+  return count == 1 ? true : false
 end
